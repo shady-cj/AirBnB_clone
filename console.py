f621b9dde8cb576247094ecd4e71f9ff17d21ca2
shady-cj <petersp2000@gmail.com>
2022-10-25 21:38:15 +0100

    improving console.py

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ This module serves as the entry point into the package
 provides a command prompt to interact with
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +20,42 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def do_create(self, cls):
+        """Creates a new instance of BaseModel class\n"""
+        if not cls:
+            print("** class name missing **")
+        else:
+            if cls != "BaseModel":
+                print("** class doesn't exist **")
+            else:
+                b = BaseModel()
+                b.save()
+                print(b.id)
+    
+    def do_show(self, line):
+        """prints the string representation of an instance\
+based on the class name and id\n"""
+
+        split_line = line.split(" ")
+        cls = split_line[0].strip()
+        if not cls:
+            print("** class name missing **")
+        else:
+            if cls != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(split_line) < 2:
+                print("** instance id missing **")
+            else:
+                id = split_line[1].strip()
+                present = False
+                print(storage.all())
+                for key in storage.all().keys():
+                    if f"BaseModel.{id}" == key:
+                        present = True
+                        b = BaseModel(**storage.all()[key])
+                        print(b)
+                if not present:
+                    print("** no instance found **")
     def emptyline(self):
         """Handles emptyline commands\n"""
         print(end="")
